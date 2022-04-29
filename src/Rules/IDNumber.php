406659d00ccf5441a2a632bd028f85fcb148b247
seffeng <seffeng@sina.cn>
2022-04-29 15:27:29 +0800

    增加密码等级验证

--- src/Rules/IDNumber.php
+++ src/Rules/IDNumber.php
@@ -12,6 +12,12 @@ use Seffeng\Rules\IDNumber as IDNumberRule;
  * 身份证号验证[仅支持18位]
  * @author zxf
  * @date   2021年07月01日
+ * @method static bool getIsStrict()
+ * @method static string getLocation()
+ * @method static string getBirthday()
+ * @method static string getGender()
+ * @method static string getGenderName()
+ * @method static \DateInterval getYears()
  */
 class IDNumber implements Rule
 {
@@ -28,6 +34,12 @@ class IDNumber implements Rule
     protected $isStrict = true;
 
     /**
+     *
+     * @var IDNumberRule
+     */
+    protected $validator;
+
+    /**
      * Determine if the validation rule passes.
      *
      * @param  string  $attribute
@@ -36,9 +48,9 @@ class IDNumber implements Rule
      */
     public function passes($attribute, $value)
     {
-        $rule = (new IDNumberRule($this->regex));
-        $rule->setIsStrict($this->isStrict);
-        return $rule->passes($value);
+        $this->validator = (new IDNumberRule($this->regex));
+        $this->getValidator()->setIsStrict($this->isStrict);
+        return $this->getValidator()->passes($value);
     }
 
     /**
@@ -49,5 +61,34 @@ class IDNumber implements Rule
     public function message()
     {
         return ':attribute格式错误！';
+    }
+
+    /**
+     *
+     * @author zxf
+     * @date   2022年4月29日
+     * @return IDNumberRule
+     */
+    protected function getValidator()
+    {
+        return $this->validator;
+    }
+
+    /**
+     *
+     * @author zxf
+     * @date   2022年4月29日
+     * @param  string $method
+     * @param  mixed $parameters
+     * @throws \Exception
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        if (method_exists($this->getValidator(), $method)) {
+            return $this->getValidator()->{$method}(...$parameters);
+        } else {
+            throw new \Exception('方法｛' . $method . '｝不存在！');
+        }
     }
 }
--- src/Rules/Password.php
+++ src/Rules/Password.php
@@ -12,6 +12,13 @@ use Seffeng\Rules\Password as PasswordRule;
  * 密码验证
  * @author zxf
  * @date    2020年05月21日
+ * @method  static PasswordRule setNumber(int $number = null)
+ * @method  static integer getNumber()
+ * @method  static PasswordRule setLevel(int $level = null)
+ * @method  static integer getIsLevel()
+ * @method  static boolean getIsLevelII()
+ * @method  static boolean getIsLevelIII()
+ * @method  static boolean getIsLevelIV()
  */
 class Password implements Rule
 {
@@ -22,6 +29,24 @@ class Password implements Rule
     protected $regex;
 
     /**
+     *
+     * @var integer
+     */
+    protected $level;
+
+    /**
+     *
+     * @var integer
+     */
+    protected $number;
+
+    /**
+     *
+     * @var PasswordRule
+     */
+    protected $validator;
+
+    /**
      * Determine if the validation rule passes.
      *
      * @param  string  $attribute
@@ -30,7 +55,10 @@ class Password implements Rule
      */
     public function passes($attribute, $value)
     {
-        return (new PasswordRule($this->regex))->passes($value);
+        $this->validator = new PasswordRule($this->regex);
+        $this->getValidator()->setLevel($this->level);
+        $this->getValidator()->setNumber($this->number);
+        return $this->getValidator()->passes($value);
     }
 
     /**
@@ -41,5 +69,34 @@ class Password implements Rule
     public function message()
     {
         return ':attribute格式错误！';
+    }
+
+    /**
+     *
+     * @author zxf
+     * @date   2022年4月29日
+     * @return PasswordRule
+     */
+    protected function getValidator()
+    {
+        return $this->validator;
+    }
+
+    /**
+     *
+     * @author zxf
+     * @date   2022年4月29日
+     * @param  string $method
+     * @param  mixed $parameters
+     * @throws \Exception
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        if (method_exists($this->getValidator(), $method)) {
+            return $this->getValidator()->{$method}(...$parameters);
+        } else {
+            throw new \Exception('方法｛' . $method . '｝不存在！');
+        }
     }
 }
